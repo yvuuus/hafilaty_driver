0b7a91e3e936a7d09b3f31cba0dd6bb0f9ffd4a8
yvuuus <152425023+yvuuus@users.noreply.github.com>
2025-01-01 00:50:06 +0100

    map integration resources 1

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -7,6 +7,7 @@ import Flutter
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
+    GMSServices.provideAPIKey("AIzaSyDULR1PxZjlXxdnV4-Btx_ZF3WFf1ocsYw")
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
